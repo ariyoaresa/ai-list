b87a8de5f4ec190ad5d7c1f1f612f48a5eec66a7
Ariyo Aresa <108690633+ariyoaresa@users.noreply.github.com>
2024-03-20 15:00:49 -0700

    Update getai.js

--- getai.js
+++ getai.js
@@ -2,7 +2,7 @@ function getList() {
     fetch('https://ariyoaresa.me/ai-list/ai/list.js')
         .then(response => response.json())
         .then(data => {
-            console.log(data[1]);
+            console.log(data);
         });
 }
 
