df1ad24f28842ffc4d130a6cdb2f70a698ac4a8e
Ariyo Aresa <108690633+ariyoaresa@users.noreply.github.com>
2024-03-20 13:46:56 -0700

    Create list.js

---
+++ ai/list.js
@@ -0,0 +1,99 @@
+[
+    {
+        "id": 1,
+        "name": "ChatGPT",
+        "description": "OpenAI's GPT-3 powered chatbot",
+        "url": "https://chat.openai.com/"
+    },
+    {
+        "id": 2,
+        "name": "Ideogram",
+        "description": "A tool for generating images from text prompts",
+        "url": "https://ideogram.io/"
+    },
+    {
+        "id": 3,
+        "name": "MidJourney",
+        "description": "A text to image AI tool",
+        "url": "https://midjourney.com/"
+    },
+    {
+        "id": 4,
+        "name": "Dall-E 2",
+        "description": "A tool for generating images from text prompts",
+        "url": "https://openai.com/dall-e-2/"
+    },
+    {
+        "id": 5,
+        "name": "Google Gemini",
+        "description": "simulate human conversations using natural language processing (NLP) and machine learning",
+        "url": "https://gemini.gooogle.com"
+    },
+    {
+        "id": 6,
+        "name": "Microsoft Copilot",
+        "description": "Microsoft Copilot leverages the power of AI to boost productivity, unlock creativity, and helps you understand information better with a simple chat experience.",
+        "url": "https://copilot.microsoft.com/"
+    },
+    {
+        "id": 7,
+        "name": "Github copilot",
+        "description": "GitHub Copilot is an AI pair programmer that helps you write code faster and with less effort. With GitHub Copilot, get suggestions for whole lines or entire functions right inside your editor.",
+        "url": "https://copilot.github.com/"
+    },
+    {
+        "id": 8,
+        "name": "Bing Image Creator",
+        "description": "Bing Image Creator is a tool that uses AI to generate images based on your text descriptions.",
+        "url": "https://bing.com/images/create"
+    },
+    {
+        "id": 9,
+        "name": "Stable Diffusion 2",
+        "description": "Unlike Dall-E 2, its source code, as well as details on the training data and weighting used by its algorithms, are openly available to the public, and the application can be downloaded and installed on your own computer",
+        "url": "https://stability.ai"
+    },
+    {
+        "id": 10,
+        "name": "Lumen5",
+        "description": "Lumen5 is a video creation platform powered by A.I. that enables anyone without training or experience to easily create engaging video content within minutes.",
+        "url": "https://lumen5.com/"
+    },
+    {
+        "id": 11,
+        "name": "Soundraw",
+        "description": "Soundraw is a tool that uses AI to generate music based on your text descriptions.",
+        "url": "https://soundraw.io/"
+    },
+    {
+        "id": 12,
+        "name": "Podcastle",
+        "description": "An audio recording and editing platform with integrated AI tools that helps you create clear, super-smooth recordings that sound as if they’ve been edited professionally, automating tasks like cleaning up messy sounds and creating transcripts",
+        "url": "https://podcastle.ai/"
+    },
+    {
+        "id": 13,
+        "name": "Cleanup.Pictures",
+        "description": "Cleanup.Pictures is a tool that uses AI to remove unwanted objects from your images.",
+        "url": "https://cleanup.pictures/"
+    },
+    {
+        "id": 14,
+        "name": "Adobe Firefly",
+        "description": "Firefly is a new AI-powered feature in Adobe Photoshop that helps you create stunning visuals in seconds. Firefly uses the power of AI to help you create stunning visuals in seconds. It’s like having a professional designer at your fingertips.",
+        "url": "https://firefly.adobe.com"
+    },
+    {
+        "id": 15,
+        "name": "remove.bg",
+        "description": "remove.bg is a tool that uses AI to remove the background from your images.",
+        "url": "https://www.remove.bg/"
+    },
+    {
+        "id": 16,
+        "name": "Grok",
+        "description": "Grok is a generative artificial intelligence chatbot developed by xAI, based on a large language model (LLM).",
+        "url": "https://grok.x.ai/"
+    },
+
+]
