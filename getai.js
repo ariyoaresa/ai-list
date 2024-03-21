20b92856773d4aee8665b9069af6b9c31a8101a5
Ariyo Aresa <108690633+ariyoaresa@users.noreply.github.com>
2024-03-21 00:33:12 -0700

    Update getai.js

--- getai.js
+++ getai.js
@@ -1,8 +1,27 @@
 function getList() {
     fetch('https://ariyoaresa.me/ai-list/ai/list.js')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
-            console.log(data);
+            let list = document.getElementById('list');
+            let listHTML = "";
+            data.forEach(item => {
+                listHTML += `
+                <div class="list">
+                    <h1 class="name">${item.name}</h1>
+                    <p class="description">${item.description}</p>
+                    <button class="itview"><a href="${item.url}" target="_blank">View</a></button>
+                </div>
+                `;
+            });
+            list.innerHTML = listHTML;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
         });
 }
 
