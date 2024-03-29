2d1089ce165237859090d175709b813355f1fc44
ChristianLudovic <talchrist10@gmail.com>
2024-03-29 04:55:57 +0300

    website redesign

--- ai/list.js
+++ ai/list.js
@@ -3,115 +3,134 @@
         "id": 1,
         "name": "ChatGPT",
         "description": "OpenAI's GPT-3 powered chatbot",
-        "url": "https://chat.openai.com/"
+        "url": "https://chat.openai.com/",
+        "logo": "./assets/Ai-icons/chatgpt-icon.svg"
     },
     {
         "id": 2,
         "name": "Ideogram",
         "description": "A tool for generating images from text prompts",
         "url": "https://ideogram.io/"
+
     },
     {
         "id": 3,
         "name": "MidJourney",
         "description": "A text to image AI tool",
-        "url": "https://midjourney.com/"
+        "url": "https://midjourney.com/",
+        "logo": "./assets/Ai-icons/midjourney-color-icon.svg"
     },
     {
         "id": 4,
         "name": "Dall-E 2",
         "description": "A tool for generating images from text prompts",
-        "url": "https://openai.com/dall-e-2/"
+        "url": "https://openai.com/dall-e-2/",
+        "logo": "./assets/Ai-icons/openai-icon.svg"
     },
     {
         "id": 5,
         "name": "Google Gemini",
         "description": "simulate human conversations using natural language processing (NLP) and machine learning",
-        "url": "https://gemini.gooogle.com"
+        "url": "https://gemini.gooogle.com",
+        "logo": "./assets/Ai-icons/google-gemini-icon.svg"
     },
     {
         "id": 6,
         "name": "Microsoft Copilot",
-        "description": "Microsoft Copilot leverages the power of AI to boost productivity, unlock creativity, and helps you understand information better with a simple chat experience.",
-        "url": "https://copilot.microsoft.com/"
+        "description": "Microsoft Copilot: AI-enhanced productivity, creativity, and comprehension via intuitive chat interface.",
+        "url": "https://copilot.microsoft.com/",
+        "logo": "./assets/Ai-icons/copilot-icon.svg"      
     },
     {
         "id": 7,
         "name": "Github copilot",
         "description": "GitHub Copilot is an AI pair programmer that helps you write code faster and with less effort. With GitHub Copilot, get suggestions for whole lines or entire functions right inside your editor.",
-        "url": "https://copilot.github.com/"
+        "url": "https://copilot.github.com/",
+        "logo": "./assets/Ai-icons/github_copilot.svg"
     },
     {
         "id": 8,
         "name": "Bing Image Creator",
         "description": "Bing Image Creator is a tool that uses AI to generate images based on your text descriptions.",
-        "url": "https://bing.com/images/create"
+        "url": "https://bing.com/images/create",
+        "logo": "./assets/Ai-icons/bing-color-icon.svg"
     },
     {
         "id": 9,
         "name": "Stable Diffusion 2",
         "description": "Unlike Dall-E 2, its source code, as well as details on the training data and weighting used by its algorithms, are openly available to the public, and the application can be downloaded and installed on your own computer",
-        "url": "https://stability.ai"
+        "url": "https://stability.ai",
+        "logo": "./assets/Ai-icons/stability-ai-icon.svg"
     },
     {
         "id": 10,
         "name": "Lumen5",
         "description": "Lumen5 is a video creation platform powered by A.I. that enables anyone without training or experience to easily create engaging video content within minutes.",
-        "url": "https://lumen5.com/"
+        "url": "https://lumen5.com/",
+        "logo": "./assets/Ai-icons/lumen5.png"
     },
     {
         "id": 11,
         "name": "Soundraw",
         "description": "Soundraw is a tool that uses AI to generate music based on your text descriptions.",
-        "url": "https://soundraw.io/"
+        "url": "https://soundraw.io/",
+        "logo": "./assets/Ai-icons/sandrow.jpg"
     },
     {
         "id": 12,
         "name": "Podcastle",
         "description": "An audio recording and editing platform with integrated AI tools that helps you create clear, super-smooth recordings that sound as if they’ve been edited professionally, automating tasks like cleaning up messy sounds and creating transcripts",
-        "url": "https://podcastle.ai/"
+        "url": "https://podcastle.ai/",
+        "logo": "./assets/Ai-icons/Podcastle-Logo-Vector.png"
     },
     {
         "id": 13,
         "name": "Cleanup.Pictures",
         "description": "Cleanup.Pictures is a tool that uses AI to remove unwanted objects from your images.",
-        "url": "https://cleanup.pictures/"
+        "url": "https://cleanup.pictures/",
+        "logo": "./assets/Ai-icons/cleanup-pictures.svg"
     },
     {
         "id": 14,
         "name": "Adobe Firefly",
         "description": "Firefly is a new AI-powered feature in Adobe Photoshop that helps you create stunning visuals in seconds. Firefly uses the power of AI to help you create stunning visuals in seconds. It’s like having a professional designer at your fingertips.",
-        "url": "https://firefly.adobe.com"
+        "url": "https://firefly.adobe.com",
+        "logo": "./assets/Ai-icons/Adobe-Firefly-Icon.svg"
     },
     {
         "id": 15,
         "name": "remove.bg",
         "description": "remove.bg is a tool that uses AI to remove the background from your images.",
-        "url": "https://www.remove.bg/"
+        "url": "https://www.remove.bg/",
+        "logo": "./assets/Ai-icons/Remove.bg_icon.png"
     },
     {
         "id": 16,
         "name": "Grok",
         "description": "Grok is a generative artificial intelligence chatbot developed by xAI, based on a large language model (LLM).",
-        "url": "https://grok.x.ai/"
+        "url": "https://grok.x.ai/",
+        "logo": "./assets/Ai-icons/grok-chatbot.svg"
     },
     {
         "id": 17,
         "name": "DeepAI",
         "description": "an innovative online platform and API service that harnesses the power of artificial intelligence (AI) to provide users with a wide range of creative and practical tools.",
-        "url": "https://deepai.org/"
+        "url": "https://deepai.org/",
+        "logo": "./assets/Ai-icons/deepai.svg"
     },
     {
         "id": 18,
         "name": "SEO ai",
         "description": "SEO.ai let's you harness the superpowers of world-leading generative artificial intelligence for generating high-quality SEO keyword research and AI copywriting.",
-        "url": "https://app.seo.ai"
+        "url": "https://app.seo.ai",
+        "logo": "./assets/Ai-icons/seo-ai.svg"
     },
     {
         "id": 19,
         "name": "Designs.AI",
         "description": "Create astonishing designs with artificial intelligence, ranging from logos to mockups",
-        "url": "https://designs.ai"
+        "url": "https://designs.ai",
+        "logo": "./assets/Ai-icons/designs-ai.png"
     }
 
 ]
